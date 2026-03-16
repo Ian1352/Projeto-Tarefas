f01737b2f42b21cd6ddefbaf86248745977bc69e
Ian Gabriel <ian-gabriel09@hotmail.com>
2026-03-16 03:27:03 -0300

    primeiro commit

---
+++ Versão_0.py
@@ -0,0 +1,34 @@
+tarefas = []
+
+def adicionarTarefas():
+    tarefa = input('Digite a tarefa: ')
+    tarefas.append(tarefa)
+
+def listarTarefas():
+    for i in (tarefas):
+        print(f'Tarefas: {i}')
+
+def sair():
+    saida = (input('Aperte "Enter" para sair'))
+
+while True:
+    usrAcao = int(input(('\n1 - Adicionar tarefa\n2 - Listar tarefa\n3 - Sair\n\nDigite a opção de interesse: ')))
+    if usrAcao == 1:
+        qtdTarefas = int(input('Quantas tarefas deseja inserir? '))
+        if qtdTarefas > 0:
+            for i in range(qtdTarefas):
+                adicionarTarefas()
+    elif usrAcao == 2:
+        if tarefas == []:
+            print('')
+            print('Sem tarefas\n')
+            sair()
+        else:
+            print('')
+            listarTarefas()
+            print('')
+            sair()
+    elif usrAcao == 3:
+        print('Finalizado')
+        break
+
